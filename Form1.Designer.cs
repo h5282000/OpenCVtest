571ca6282a46913702e7f9e77de9b64d5371ef26
진성 한 <h5282001@naver.com>
2021-06-07 14:43:04 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace OpenCvtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgupload = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.OrignalPic = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.MatchingPic = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.ther = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrignalPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MatchingPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgupload
+            // 
+            this.imgupload.Location = new System.Drawing.Point(12, 12);
+            this.imgupload.Name = "imgupload";
+            this.imgupload.Size = new System.Drawing.Size(104, 22);
+            this.imgupload.TabIndex = 0;
+            this.imgupload.Text = "이미지 업로드";
+            this.imgupload.UseVisualStyleBackColor = true;
+            this.imgupload.Click += new System.EventHandler(this.imgupload_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "잘라낸 이미지";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 22);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "매칭하기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(-23, -46);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxIpl1.TabIndex = 3;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // OrignalPic
+            // 
+            this.OrignalPic.Location = new System.Drawing.Point(141, 12);
+            this.OrignalPic.Name = "OrignalPic";
+            this.OrignalPic.Size = new System.Drawing.Size(713, 507);
+            this.OrignalPic.TabIndex = 4;
+            this.OrignalPic.TabStop = false;
+            // 
+            // MatchingPic
+            // 
+            this.MatchingPic.Location = new System.Drawing.Point(880, 40);
+            this.MatchingPic.Name = "MatchingPic";
+            this.MatchingPic.Size = new System.Drawing.Size(228, 149);
+            this.MatchingPic.TabIndex = 5;
+            this.MatchingPic.TabStop = false;
+            // 
+            // ther
+            // 
+            this.ther.Location = new System.Drawing.Point(894, 247);
+            this.ther.Name = "ther";
+            this.ther.Size = new System.Drawing.Size(100, 21);
+            this.ther.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 551);
+            this.Controls.Add(this.ther);
+            this.Controls.Add(this.MatchingPic);
+            this.Controls.Add(this.OrignalPic);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imgupload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrignalPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MatchingPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button imgupload;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private OpenCvSharp.UserInterface.PictureBoxIpl OrignalPic;
+        private OpenCvSharp.UserInterface.PictureBoxIpl MatchingPic;
+        private System.Windows.Forms.TextBox ther;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+namespace OpenCvtest
+{
+    public partial class Form1 : Form
+    {
+        string Filepath = "";
+        string Matchingpath = "";
+        string Matchingfilename;
+        Mat Result = new Mat();
+        Mat Orignalimg = new Mat();
+        Mat Matchingimg = new Mat();
+        Mat Resizeimg = new Mat();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void OrignalFileOpen()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.DefaultExt = "png";
+
+            openFile.Filter = "Graphics interchange Format (*.png)|*.png|(*.jpg)|*.jpg|All files(*.*)|*.*";
+
+            openFile.ShowDialog();
+
+            if (openFile.FileName.Length > 0)
+            {
+                Image image1 = Image.FromFile(openFile.FileName);
+                Filepath = openFile.FileName;
+                Orignalimg = Cv2.ImRead(Filepath, ImreadModes.Grayscale);
+
+
+                Cv2.GaussianBlur(Orignalimg, Orignalimg, new OpenCvSharp.Size(5, 5), 0); // 가우시안 블러 적용
+//              Cv2.BilateralFilter(Orignalimg, Orignalimg, 9, 3, 3, BorderTypes.Default); //리사이즈 시  쌍방필터 적용
+//              Cv2.MedianBlur(Orignalimg, Orignalimg, 15); // 리사이즈 시 메디안 블러 적용
+//              Cv2.BoxFilter(Orignalimg, Orignalimg, MatType.CV_8UC1, new OpenCvSharp.Size(3, 3), new OpenCvSharp.Point(0, 0), true, BorderTypes.Default); //리사이즈 시 박스 필터 적용
+//              Cv2.Blur(Orignalimg, Orignalimg, new OpenCvSharp.Size(3, 3)); // 리사이즈 시 단순 블러
+
+
+                OrignalPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                OrignalPic.Image = image1;
+            }
+            else
+                return;
+        }
+        public void TemplateFileOpen()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.DefaultExt = "png";
+
+            openFile.Filter = "Graphics interchange Format (*.png)|*.png|(*.jpg)|*.jpg|All files(*.*)|*.*";
+
+            openFile.ShowDialog();
+
+            if (openFile.FileName.Length > 0)
+            {
+                Image image1 = Image.FromFile(openFile.FileName);
+                Filepath = openFile.FileName;
+                Matchingimg = Cv2.ImRead(Filepath, ImreadModes.Grayscale);
+                MatchingPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                MatchingPic.Image = image1;
+            }
+            else
+                return;
+        }
+
+        public void Matching()
+        {
+            var threshold = 0.8; // 유사도 설정
+
+
+            Cv2.Resize(Matchingimg, Resizeimg, new OpenCvSharp.Size(Convert.ToDouble(Orignalimg.Width) * (Convert.ToDouble(100)/ Convert.ToDouble(125)), Convert.ToDouble(Orignalimg.Height) * (Convert.ToDouble(100)/ Convert.ToDouble(125)))); // 리사이즈 테스트 0.8배율
+            Cv2.GaussianBlur(Resizeimg, Resizeimg, new OpenCvSharp.Size(5, 5), 0); // 리사이즈 시 가우시안 블러 적용
+//          Cv2.BilateralFilter(Resizeimg, Resizeimg, 9, 3, 3, BorderTypes.Default); //리사이즈 시  쌍방필터 적용
+//          Cv2.MedianBlur(Resizeimg, Resizeimg, 15); // 리사이즈 시 메디안 블러 적용
+//          Cv2.BoxFilter(Resizeimg, Resizeimg, MatType.CV_8UC1, new OpenCvSharp.Size(3, 3), new OpenCvSharp.Point(0, 0), true, BorderTypes.Default); //리사이즈 시 박스 필터 적용
+//          Cv2.Blur(Resizeimg, Resizeimg, new OpenCvSharp.Size(3, 3)); // 리사이즈 시 단순 블러
+            Cv2.MatchTemplate(Orignalimg, Resizeimg, Result, TemplateMatchModes.CCoeffNormed); // 리사이즈 시 탬플릿 매칭
+
+
+//            Cv2.MatchTemplate(Orignalimg, Matchingimg, Result, TemplateMatchModes.CCoeffNormed); // 기본 탬플릿 매칭
+
+
+            double minval, maxval;
+            OpenCvSharp.Point minloc, maxloc;
+
+            Cv2.MinMaxLoc(Result, out minval, out maxval, out minloc, out maxloc);
+            ther.Text = maxval.ToString();
+            if (maxval >= threshold)
+            {
+                Rect rect = new Rect(maxloc.X, maxloc.Y, Resizeimg.Width, Resizeimg.Height); // 리사이즈 시 사각형 생성
+
+//              Rect rect = new Rect(maxloc.X, maxloc.Y, Matchingimg.Width, Matchingimg.Height);
+
+                Cv2.Rectangle(Orignalimg, rect, Scalar.Red, 1);
+                Cv2.Rectangle(Result, rect, new OpenCvSharp.Scalar(0, 0, 255), 1);
+                OrignalPic.ImageIpl = Orignalimg;
+            }
+            MatchingPic.ImageIpl = Resizeimg; // 리사이즈 시 해당 이미지 변경
+//          MatchingPic.ImageIpl = Matchingimg;
+
+        }
+
+        private void imgupload_Click(object sender, EventArgs e) // 원본 이미지
+        {
+            OrignalFileOpen();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // 잘라낸 이미지
+        {
+            TemplateFileOpen();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // 매칭
+        {
+            Matching();
+        }
+    }
+}
